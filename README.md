92c0976dc98a89d0e41a19d3819e7817515ef286
omer <omcdnr@gmail.com>
2018-04-27 20:30:34 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# putty-filezilla-auto-login
+
+Powershell script for automatically starting and logging in to PuTTY and FileZilla sessions. You can add commands to be automatically run after
+you are logged in. 
---
+++ autostart.ps1
@@ -0,0 +1,3 @@
+﻿﻿Start-process -FilePath 'C:\Program Files\PuTTY\PuTTY.exe' '-load <your-saved-session-name> -l <session-login-name> -pw <session-password> -t -m "<path to commands>"'
+Start-process -FilePath 'C:\Program Files\FileZilla FTP Client\filezilla.exe' "-c 0/<your saved site name >"
+stop-process -Id $PID
